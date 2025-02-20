1ed414dc789ce5edea9b15f75c22935b3257e4d1
Tiago Vattos Zamboni <tiagovattosz@gmail.com>
2025-02-19 21:59:59 -0300

    add new nav item

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import NavItem from "./NavItem";
 export default function Header() {
   return (
-    <header className="container mx-auto flex justify-between items-center text-lg h-15">
+    <header className="container mx-auto flex justify-between items-center h-15">
       <Link to="/">Tvz.</Link>
       <nav>
         <ul className="flex gap-8">
@@ -10,6 +10,7 @@ export default function Header() {
           <NavItem to="/about">About</NavItem>
           <NavItem to="/projects">Projects</NavItem>
           <NavItem to="/resume">Resume</NavItem>
+          <NavItem to="/resume">Contact</NavItem>
         </ul>
       </nav>
     </header>
